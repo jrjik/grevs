910e43fb64af2132ffc566328ac065c7e05a5e70
root <root@7835334-ey016275.twc1.net>
2026-05-08 14:46:18 +0000

    Add https on server

--- project/core/settings.py
+++ project/core/settings.py
@@ -23,10 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+DEBUG = config('DEBUG', default='False') == 'True' 
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
